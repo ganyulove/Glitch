d68976b9af22062f31fbfd3dff4315b3a450dfbf
ganyulove <52892624+ganyulove@users.noreply.github.com>
2023-03-12 04:13:32 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const username = process.env.WEB_USERNAME || "admin";
-const password = process.env.WEB_PASSWORD || "password";
+const username = process.env.WEB_USERNAME || "admin_";
+const password = process.env.WEB_PASSWORD || "password_";
 const url = "https://" + process.env.PROJECT_DOMAIN + ".glitch.me";
 const port = process.env.PORT || 3000;
 const express = require("express");
